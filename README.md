c3f34ce7fb3262d4ae2873a42ec58f878e6eb1e6
Alastair Coote <alastair.coote@nytimes.com>
2020-08-06 21:00:54 -0400

    first push

---
+++ README.md
@@ -0,0 +1,19 @@
+# quickjs-android-suitable-sys
+
+FFI Bindings for [quickjs](https://bellard.org/quickjs/). One of [many](https://crates.io/search?q=quickjs), but I created this one as a fork of [libquickjs-sys](https://crates.io/crates/libquickjs-sys) becuse it wasn't working properly on i686 Android.
+
+## What?
+
+The i686 toolchain is used when building for the Android emulator and more or less nothing else these days. It turns out that [bindgen](https://github.com/rust-lang/rust-bindgen) isn't aware of compilation target by default and for some reason the code generated for 64bit OSes will crash on i686 Android (and maybe others). This crate rebuilds the bindings whenever the library is built, ensuring platform consistency.
+
+Making this change breaks compatibility with the higher level library used by libquickjs-sys, so I made a fresh fork.
+
+## Extra features
+
+- `bignum`
+  
+  Enable this to add bigint support to QuickJS
+
+- `dump_leaks`
+
+  Dumps out variables still being held in the JS engine whenever it is released. Useful to debugging memory leaks.
---
+++ build.rs
@@ -0,0 +1,94 @@
+use std::path::{PathBuf};
+use std::env;
+
+fn main() {
+    build_library();
+    generate_bindings();
+}
+
+fn build_library() {
+
+    let code_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("quickjs");
+    
+    let quickjs_version =
+        std::fs::read_to_string(code_dir.join("VERSION")).expect("failed to read quickjs version");
+
+    let mut builder = cc::Build::new();
+
+    #[cfg(feature = "dump_leaks")]
+    builder.define("DUMP_LEAKS", None);
+
+    #[cfg(feature = "bignum")]
+    builder.define("CONFIG_BIGNUM", None);
+
+    builder.files(
+        [
+            "cutils.c",
+            "libbf.c",
+            "libregexp.c",
+            "libunicode.c",
+            "quickjs.c",
+        ]
+        .iter()
+        .map(|f| code_dir.join(f)),
+    ).define("_GNU_SOURCE", None)
+        .define(
+            "CONFIG_VERSION",
+            format!("\"{}\"", quickjs_version.trim()).as_str(),
+        )
+        
+        // The below flags are used by the official Makefile.
+        .flag_if_supported("-Wchar-subscripts")
+        .flag_if_supported("-Wno-array-bounds")
+        .flag_if_supported("-Wno-format-truncation")
+        .flag_if_supported("-Wno-missing-field-initializers")
+        .flag_if_supported("-Wno-sign-compare")
+        .flag_if_supported("-Wno-unused-parameter")
+        .flag_if_supported("-Wundef")
+        .flag_if_supported("-Wuninitialized")
+        .flag_if_supported("-Wunused")
+        .flag_if_supported("-Wwrite-strings")
+        .flag_if_supported("-funsigned-char")
+        // Below flags are added to supress warnings that appear on some
+        // platforms.
+        .flag_if_supported("-Wno-cast-function-type")
+        .flag_if_supported("-Wno-implicit-fallthrough")
+        // cc uses the OPT_LEVEL env var by default, but we hardcode it to -O2
+        // since release builds use -O3 which might be problematic for quickjs,
+        // and debug builds only happen once anyway so the optimization slowdown
+        // is fine.
+        .opt_level(2)
+        .compile("quickjs");
+}
+
+
+
+fn generate_bindings() {
+    // Turns out QuickJS bindings look really different when you're building for
+    // Android i686. Why? No idea! But bindgen doesn't check platform, so we have to manually
+    // specify it.
+    let target = env::var("TARGET").expect("Could not read the target platform");
+
+    let header_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("quickjs+extern.h");
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    
+    // Generate bindings.
+    let bindings = bindgen::Builder::default()
+        .header(header_path.to_str().expect("Could not create QuickJS path").to_string())
+        .clang_arg(format!("--target={}",target))
+        // When in Android i686 JSValue becomes an f64. So we force JSValue to be
+        // opaque in all other situations, so we don't try to read out tags
+        // or anything like that and suddenly discover things break on i686.
+        // .opaque_type("JSValue")
+        .whitelist_function("JS.*")
+        .whitelist_type("JS.*")
+        .whitelist_var("JS.*")
+        .size_t_is_usize(true)
+        .generate()
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
